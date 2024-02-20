b4fc12e958822258df1966c305d0c12a8e8b83aa
agahafiz04 <agahafiz04@gmail.com>
2024-02-20 07:41:58 +0700

    Added main components and some basic styling

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import "../styles/extra.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        Copyright @agahafiz <i className="devicon-github-original colored"></i>
+      </p>
+    </footer>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import "../styles/extra.css";
+
+export default function Title() {
+  return (
+    <header>
+      <h1>CV Generator</h1>
+      <p>CV Generator Can Create Your Profile With Ease</p>
+    </header>
+  );
+}
